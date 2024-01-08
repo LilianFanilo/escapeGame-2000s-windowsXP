aa65d4c303e4055eba4b7c5a3e58d53797aa1f28
Lilian <lilianpeuron@gmail.com>
2024-01-09 00:04:21 +0100

    Create Office

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Office from "./assets/Office/Office";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Office />
     </>
   );
 }
---
+++ src/assets/Office/Office.jsx
@@ -0,0 +1,18 @@
+import s from "./Office.module.scss";
+
+function Office() {
+  return (
+    <>
+      <div className={s.officeComponent}>
+        <footer className={s.bottomBar}>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div></div>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default Office;
