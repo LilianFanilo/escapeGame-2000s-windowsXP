63ccdee8178ee870c8b7843d1d9e75a677e8411b
Lilian <lilianpeuron@gmail.com>
2024-01-10 12:16:51 +0100

    LoginPage Working progress 10/01/2024 12:16

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import LoginPage from "./components/LoginPage/LoginPage";
 import WindowsXP from "./components/WindowsXP/WindowsXP";
 
 function App() {
   return (
     <>
+      <LoginPage />
       <WindowsXP />
     </>
   );
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -3,7 +3,30 @@ import s from "./LoginPage.module.scss";
 function LoginPage() {
   return (
     <>
-      <div className={s.loginPageComponent}></div>
+      <div className={s.loginPageComponent}>
+        <div className={s.loginPageMain}>
+          <div>
+            <span>To begin, click your user name</span>
+          </div>
+          <div>
+            <img
+              className={s.profileIMG}
+              src=""
+              alt=""
+              width={60}
+              height={60}
+            />
+            <span>Elodie</span>
+          </div>
+        </div>
+        <div className={s.loginPageBottom}>
+          <div></div>
+          <div>
+            After you log on, you can add or change accounts. <br /> Just go to
+            Control Panel and click User Accounts.
+          </div>
+        </div>
+      </div>
     </>
   );
 }
