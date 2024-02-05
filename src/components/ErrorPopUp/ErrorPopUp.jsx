df7b7500cc48f60da44957a24b4b2b02c9267cbe
Valentin Jullien <98819990+Valentin667@users.noreply.github.com>
2024-02-05 16:08:36 +0100

    Add sound when the component is trigger and close possibility

--- src/components/ErrorPopUp/ErrorPopUp.jsx
+++ src/components/ErrorPopUp/ErrorPopUp.jsx
@@ -1,47 +1,61 @@
+import { useState } from "react";
 import s from "./ErrorPopUp.module.scss"
 import error from "/images/buttons/error.png";
 import error_main from "/images/buttons/error_main.png";
 
-export default function ErrorPopUp() {
+export default function ErrorPopUp({ onClose }) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleConfirmClick = () => {
+    // Logique à exécuter lors du clic sur le bouton OK
+    // ...
+
+    // Fermer le composant en mettant isVisible à false
+    setIsVisible(false);
+    onClose();
+  };
+
   return (
     <>
-      <div className={s.window}>
-        <div className={s.header_bg}></div>
-        <div className={s.window_header}>
-        <img
-          draggable="false"
-          src={error}
-          className={s.window_header_icon}
-          alt=""
-        />
-          <div className={s.window_header_title}>C:\</div>
-          <div className={s.window_close}>
-            <button className={s.window_button}>
-            </button>
-          </div>
-        </div>
-        <div className={s.window_content}>
-          <div className={s.window_main}>
-            <div className={s.error_top}>
-              <img
-                draggable="false"
-                src={error_main}
-                className={s.error_image}
-                alt=""
-              />
-              <div className={s.error_messages}>
-                <p className={s.error_message}>C:\</p>
-                <p className={s.error_message}>Application not found</p>
-              </div>
-            </div>
-            <div className={s.error_bottom}>
-              <div className={s.error_button}>
-                <span className={s.error_confirm}>OK</span>
-              </div>
+      {isVisible && (
+        <div className={s.window}>
+          <div className={s.header_bg}></div>
+          <div className={s.window_header}>
+          <img
+            draggable="false"
+            src={error}
+            className={s.window_header_icon}
+            alt=""
+          />
+            <div className={s.window_header_title}>C:\</div>
+            <div className={s.window_close}>
+              <button className={s.window_button} onClick={handleConfirmClick}>
+              </button>
             </div>
           </div>
+          <div className={s.window_content}>
+            <div className={s.window_main}>
+              <div className={s.error_top}>
+                <img
+                  draggable="false"
+                  src={error_main}
+                  className={s.error_image}
+                  alt=""
+                />
+                <div className={s.error_messages}>
+                  <p className={s.error_message}>C:\</p>
+                  <p className={s.error_message}>Application not found</p>
+                </div>
+              </div>
+              <div className={s.error_bottom}>
+                <div className={s.error_button} onClick={handleConfirmClick}>
+                  <span className={s.error_confirm}>OK</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
--- src/components/WindowsXP/OfficeFooter/MenuFooter.jsx
+++ src/components/WindowsXP/OfficeFooter/MenuFooter.jsx
@@ -1,9 +1,34 @@
 import ButtonLogOff from "../../ButtonLogOff/ButtonLogOff";
 import ButtonOff from "../../ButtonOff/ButtonOff";
+import ErrorPopUp from "../../ErrorPopUp/ErrorPopUp";
 import MenuItem from "../../MenuItem/MenuItem";
 import s from "./MenuFooter.module.scss";
+import sound from "../../../assets/sounds/error.wav";
+import { useRef, useState } from "react";
 
 function MenuFooter() {
+  const [componentToShow, setComponentToShow] = useState(null);
+  const [isVisible, setIsVisible] = useState(true);
+  const audioRef = useRef(null);
+
+  const playClickSound = () => {
+    audioRef.current.play().catch((error) => {
+      console.error("Error playing audio:", error);
+    });
+  };
+
+  const handleMenuItemClick = (itemName) => {
+    playClickSound();
+    setIsVisible(true);
+  
+    // Logique pour définir le composant à afficher en fonction de l'itemName
+    if (itemName === "Internet") {
+      setComponentToShow(<ErrorPopUp className={s.center} onClose={() => setIsVisible(false)}/>);
+    } else {
+      // Gérez d'autres cas ici
+    }
+  };
+  
   return (
     <>
       <div className={s.MenuFooter}>
@@ -20,11 +45,13 @@ function MenuFooter() {
                 itemName="Internet"
                 itemSubName="Internet Explorer"
               />
-              <MenuItem
-                itemImg="/images/menu_items/email.png"
-                itemName="E-mail"
-                itemSubName="Outlook Express"
-              />
+              <div onClick={() => handleMenuItemClick("Internet")}>
+                <MenuItem
+                  itemImg="/images/menu_items/email.png"
+                  itemName="E-mail"
+                  itemSubName="Outlook Express"
+                />
+              </div>
               <div className={s.MenuItem_separator}></div>
               <MenuItem
                 itemImg="/images/menu_items/minesweeper.png"
@@ -51,6 +78,7 @@ function MenuFooter() {
                 itemName="Windows Messenger"
               />
             </div>
+            <audio ref={audioRef} src={sound} />
             <div className={s.menuRight}></div>
           </section>
           <footer>
@@ -58,6 +86,7 @@ function MenuFooter() {
             <ButtonOff />
           </footer>
         </div>
+        {isVisible && componentToShow}
       </div>
     </>
   );
