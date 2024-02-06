762e5f7faf4bbea586ea92f9f86e7a3680f4281e
Lilian <lilianpeuron@gmail.com>
2024-02-06 14:25:00 +0100

    Add AppToolBar + MyComputer app

---
+++ src/components/WindowsXP/apps/MyComputer/MyComputer.jsx
@@ -0,0 +1,16 @@
+import AppToolBar from "../../../AppToolBar/AppToolBar";
+
+function MyComputer() {
+  return (
+    <>
+      <div>
+        <div>
+          <AppToolBar />
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MyComputer;
