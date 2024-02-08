77ecb1de2c5a7d31e0ebad6dc4bfa115227728af
Valentin Jullien <98819990+Valentin667@users.noreply.github.com>
2024-02-08 14:55:08 +0100

    Update Blog

--- src/components/WindowsXP/apps/Blog/Blog.jsx
+++ src/components/WindowsXP/apps/Blog/Blog.jsx
@@ -5,13 +5,29 @@ export default function Blog() {
     <>
       <div className={s.blog}>
         <div className={s.blog_left}>
-          <img src="images/blog/blog_left/logo.png" alt="" />
-          <img src="images/blog/blog_left/britney.jpg" alt="" />
-          <img src="images/blog/blog_left/image02.jpg" alt="" />
+          <img src="images/blog/blog_left/logo.png" alt="" draggable="false" />
+          <img
+            src="images/blog/blog_left/britney.jpg"
+            alt=""
+            draggable="false"
+          />
+          <img
+            src="images/blog/blog_left/image02.jpg"
+            alt=""
+            draggable="false"
+          />
         </div>
         <div className={s.blog_center}></div>
         <div className={s.blog_right}>
-          <input type="text" placeholder="Rechercher ..." />
+          <div className={s.input_global}>
+            <input type="text" placeholder="Rechercher ..." />
+            <img
+              src="images/blog/blog_right/search.png"
+              alt=""
+              draggable="false"
+              className={s.search}
+            />
+          </div>
         </div>
       </div>
     </>
