ad808f030e870c4bb438c5a497b062712c505532
Lilian <lilianpeuron@gmail.com>
2024-04-23 10:12:48 +0200

    Upd AudioVis

--- src/components/WindowsXP/WindowsXP.jsx
+++ src/components/WindowsXP/WindowsXP.jsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import s from "./WindowsXP.module.scss";
 import OfficeFooter from "./OfficeFooter/OfficeFooter";
 import Notepad from "./apps/Notepad/Notepad";
 import ErrorPopUp from "../ErrorPopUp/ErrorPopUp";
 import Blog from "./apps/Blog/Blog";
-import blog_icone from "../../assets/WindowsIcons/blog_icone.png";
-import { useState } from "react";
 import Audiovisualiser from "./apps/Audiovisualiser/Audiovisualiser";
+import blog_icone from "../../assets/WindowsIcons/blog_icone.png";
+import audiovisualiser_icon from "../../assets/WindowsIcons/audiovisualiser_icon.png";
 
 function WindowsXP() {
   const [blogVisible, setBlogVisible] = useState(false);
+  const [audioVisible, setAudioVisible] = useState(false);
 
   const handleBlogIconDoubleClick = () => {
     setBlogVisible(true);
+  };
+
+  const handleAudioIconDoubleClick = () => {
+    setAudioVisible(true);
   };
 
   return (
@@ -33,7 +39,23 @@ function WindowsXP() {
       </div>
       {/* Affichage du composant Blog lorsque blogVisible est true */}
       {blogVisible && <Blog />}
-      <Audiovisualiser />
+
+      <div
+        className={s.audiovisualiser_icon}
+        onDoubleClick={handleAudioIconDoubleClick}
+      >
+        <img
+          draggable="false"
+          height={30}
+          width={30}
+          src={audiovisualiser_icon}
+          className={s.audiovisualiser_icon}
+          alt=""
+        />
+        <p className={s.title}>Lecteur audio</p>
+      </div>
+      {/* Affichage du composant Blog lorsque blogVisible est true */}
+      {audioVisible && <Audiovisualiser />}
     </>
   );
 }
