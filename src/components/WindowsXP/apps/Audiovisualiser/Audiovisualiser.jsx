4e1fdf49d07450a0072a33ecb1519e3ecda2193e
Lilian <lilianpeuron@gmail.com>
2024-04-26 11:56:28 +0200

    Add fonctionnalities Vid Player

--- src/components/WindowsXP/apps/Audiovisualiser/Audiovisualiser.jsx
+++ src/components/WindowsXP/apps/Audiovisualiser/Audiovisualiser.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Audiovisualiser.module.scss";
 
 const Audiovisualiser = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+
+  const playVid = () => {
+    let vid = document.getElementById("myVideo");
+    if (!isPlaying) {
+      setIsPlaying(true);
+      vid.play();
+    } else if (isPlaying) {
+      setIsPlaying(false);
+      vid.pause();
+    }
+  };
+
+  const updateProgressBar = () => {
+    let vid = document.getElementById("myVideo");
+    let progressBar = document.getElementById("progress-bar");
+    let percentage = Math.floor((100 / vid.duration) * vid.currentTime);
+    progressBar.value = percentage;
+  };
+
+  const seekTimeStamp = (event) => {
+    let vid = document.getElementById("myVideo");
+    let progressBar = document.getElementById("progress-bar");
+    let percent = event.offsetX / progressBar.offsetWidth;
+    vid.currentTime = percent * vid.duration;
+    progressBar.value = Math.floor(percent / 100);
+  };
+
+  const restartVid = () => {
+    let vid = document.getElementById("myVideo");
+    vid.pause();
+    vid.currentTime = 0;
+  };
+
+  const skip = (value) => {
+    let vid = document.getElementById("myVideo");
+    vid.currentTime += value;
+  };
+
+  const changeVolume = (evt) => {
+    let vid = document.getElementById("myVideo");
+    vid.volume = evt.target.value;
+    console.log(evt);
+  };
+
+  const muteVolume = () => {
+    let vid = document.getElementById("myVideo");
+    if (vid.muted) {
+      setIsMuted(false);
+      vid.muted = false;
+    } else {
+      setIsMuted(true);
+      vid.muted = true;
+    }
+  };
+
   return (
     <div className={s.audiovisualiser}>
       <nav>
@@ -13,52 +70,63 @@ const Audiovisualiser = () => {
           <li>Aide</li>
         </ul>
       </nav>
-      <video src=""></video>
+      {/* Appel de la vidéo */}
+      <video
+        id="myVideo"
+        src="https://www.w3schools.com/html/mov_bbb.mp4"
+        onTimeUpdate={updateProgressBar}
+      ></video>
+
       <figcaption>
         <div className={s.videoInputsLeft}>
-          <button
-            className={s.btnPlay}
-            id="play"
-            aria-label="Play"
-            role="button"
-          >
-            ►
+          {/* Button Play Pause */}
+          <button className={s.btnPlay} onClick={playVid}>
+            {isPlaying && "II"}
+            {!isPlaying && "►"}
           </button>
-          <button
-            className={s.btnStop}
-            id="play"
-            aria-label="Play"
-            role="button"
-          >
+          {/* Restart */}
+          <button className={s.btnStop} onClick={restartVid}>
             ■
           </button>
         </div>
         <div className={s.videoInputsRight}>
           <div className={s.progressContainer}>
-            <button>
+            {/* Rewind */}
+            <button onClick={() => skip(-10)}>
               <img src="/images/audiovisualiser/btn_past.svg" alt="" />
             </button>
-            <progress id="progress" max="100" value="0">
-              Progress
-            </progress>
-            <button>
+            {/* Progress bar */}
+            <progress
+              id="progress-bar"
+              min={0}
+              max={100}
+              value={0}
+              onClick={() => seekTimeStamp(event)}
+            ></progress>
+            {/* Fast Forward */}
+            <button onClick={() => skip(10)}>
               <img src="/images/audiovisualiser/btn_forward.svg" alt="" />
             </button>
           </div>
           <div className={s.btnContainer}>
             <div>
-              <button className={s.pinkBtn}>
-                <img src="/images/audiovisualiser/btn_previous.svg" alt="" />
+              {/* Volume Mute */}
+              <button id="btnMute" onClick={muteVolume}>
+                {isMuted && "Mute"}
+                {!isMuted && (
+                  <img src="/images/audiovisualiser/btn_audio.svg" alt="" />
+                )}
               </button>
-              <button className={s.pinkBtn}>
-                <img src="/images/audiovisualiser/btn_next.svg" alt="" />
-              </button>
-            </div>
-            <div>
-              <button className={s.pinkBtn}>
-                <img src="/images/audiovisualiser/btn_audio.svg" alt="" />
-              </button>
-              <progress></progress>
+              {/* Volume Changer */}
+              <input
+                type="range"
+                id="volume-bar"
+                title="volume"
+                min={0}
+                max={1}
+                step={0.1}
+                onChange={changeVolume}
+              />
             </div>
             <div>
               <span>Nom Chanson</span>
