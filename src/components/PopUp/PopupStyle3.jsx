c66e20fb6ab1470f8bd02719c4107297378cce27
Lilian <lilianpeuron@gmail.com>
2024-05-14 09:44:47 +0200

    Add popups img and sound

--- src/components/PopUp/PopupStyle3.jsx
+++ src/components/PopUp/PopupStyle3.jsx
@@ -3,9 +3,12 @@ import Window from "../Window/Window";
 import s from "./PopUp.module.scss";
 
 const PopupStyle3 = () => {
+  var audio = new Audio("/audios/mcdonalds.mp3");
+  audio.play();
+
   return (
     <Window>
-      <div className={s.PopupStyle3}>Test3</div>
+      <div className={s.PopupStyle3}></div>
     </Window>
   );
 };
--- src/components/PopUp/PopupStyle4.jsx
+++ src/components/PopUp/PopupStyle4.jsx
@@ -3,9 +3,12 @@ import Window from "../Window/Window";
 import s from "./PopUp.module.scss";
 
 const PopupStyle4 = () => {
+  var audio = new Audio("/audios/sncf.mp3");
+  audio.play();
+
   return (
     <Window>
-      <div className={s.PopupStyle4}>Test4</div>
+      <div className={s.PopupStyle4}></div>
     </Window>
   );
 };
