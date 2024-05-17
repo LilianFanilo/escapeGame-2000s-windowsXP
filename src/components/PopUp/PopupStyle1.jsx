a168c0b6f87aec11a9f287ff8c65f0203fe5a856
Lilian <lilianpeuron@gmail.com>
2024-05-17 09:50:31 +0200

    Finish adding Popups

--- src/components/PopUp/PopupStyle1.jsx
+++ src/components/PopUp/PopupStyle1.jsx
@@ -3,6 +3,9 @@ import Window from "../Window/Window";
 import s from "./PopUp.module.scss";
 
 const PopupStyle1 = () => {
+  var audio = new Audio("/audios/chocapic.mp3");
+  audio.play();
+
   return (
     <Window>
       <div className={s.PopupStyle1}></div>
--- src/components/PopUp/PopupStyle5.jsx
+++ src/components/PopUp/PopupStyle5.jsx
@@ -3,6 +3,9 @@ import Window from "../Window/Window";
 import s from "./PopUp.module.scss";
 
 const PopupStyle5 = () => {
+  var audio = new Audio("/audios/VivelleDop.mp3");
+  audio.play();
+
   return (
     <Window>
       <div className={s.PopupStyle5}></div>
