572db994192177f3ae2366ba2f32e2a30ee4bd1a
Valentin Jullien <98819990+Valentin667@users.noreply.github.com>
2024-05-24 09:41:08 +0200

    Add different focus color

--- src/components/Window/Window.jsx
+++ src/components/Window/Window.jsx
@@ -9,6 +9,7 @@ export default function Window({ onClose, children, appName, appIcon }) {
   const [isDragging, setIsDragging] = useState(false);
   const [initialPosition, setInitialPosition] = useState({ x: 0, y: 0 });
   const [isFirstLoop, setIsFirstLoop] = useState(true);
+  const [isFocused, setIsFocused] = useState(true);
   const windowRef = useRef(null);
 
   useEffect(() => {
@@ -44,8 +45,10 @@ export default function Window({ onClose, children, appName, appIcon }) {
     }
 
     const handleClickOutside = (e) => {
-      if (windowEl && !windowEl.contains(e.target)) {
-        onClose();
+      if (windowRef.current && !windowRef.current.contains(e.target)) {
+        setIsFocused(false);
+      } else {
+        setIsFocused(true);
       }
     };
 
@@ -61,6 +64,7 @@ export default function Window({ onClose, children, appName, appIcon }) {
   const handleMouseDown = (e) => {
     setIsDragging(true);
     setInitialPosition({ x: e.clientX, y: e.clientY });
+    setIsFocused(true);
   };
 
   const handleHeaderClick = (e) => {
@@ -80,10 +84,18 @@ export default function Window({ onClose, children, appName, appIcon }) {
         className={s.window}
         style={{
           transform: `translate(${windowPosition.x}px, ${windowPosition.y}px)`,
+          backgroundColor: isFocused ? "rgb(8, 49, 217)" : "rgb(101, 130, 245)",
         }}
         onMouseDown={handleMouseDown}
       >
-        <div className={s.header_bg}></div>
+        <div
+          className={s.header_bg}
+          style={{
+            background: isFocused
+              ? "linear-gradient(rgb(0, 88, 238) 0%, rgb(53, 147, 255) 4%, rgb(40, 142, 255) 6%, rgb(18, 125, 255) 8%, rgb(3, 111, 252) 10%, rgb(2, 98, 238) 14%, rgb(0, 87, 229) 20%, rgb(0, 84, 227) 24%, rgb(0, 85, 235) 56%, rgb(0, 91, 245) 66%, rgb(2, 106, 254) 76%, rgb(0, 98, 239) 86%, rgb(0, 82, 214) 92%, rgb(0, 64, 171) 94%, rgb(0, 48, 146) 100%)"
+              : "linear-gradient(rgb(118, 151, 231) 0%, rgb(126, 158, 227) 3%, rgb(148, 175, 232) 6%, rgb(151, 180, 233) 8%, rgb(130, 165, 228) 14%, rgb(124, 159, 226) 17%, rgb(121, 150, 222) 25%, rgb(123, 153, 225) 56%, rgb(130, 169, 233) 81%, rgb(128, 165, 231) 89%, rgb(123, 150, 225) 94%, rgb(122, 147, 223) 97%, rgb(171, 186, 227) 100%)",
+          }}
+        ></div>
         <header className={s.window_header} onClick={handleHeaderClick}>
           <img
             draggable="false"
