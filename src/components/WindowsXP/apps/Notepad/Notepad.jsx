1c942e59b3502872aab1a44eda613188e1c7287a
Valentin Jullien <98819990+Valentin667@users.noreply.github.com>
2024-05-29 00:30:25 +0200

    Notepad name and icon

--- src/components/WindowsXP/apps/Notepad/Notepad.jsx
+++ src/components/WindowsXP/apps/Notepad/Notepad.jsx
@@ -1,10 +1,11 @@
 import Window from "../../../Window/Window";
+import notepad_icon from "../../../../assets/WindowsIcons/notepad.png";
 import s from "./Notepad.module.scss";
 
 export default function Notepad({ onClose }) {
   return (
     <>
-      <Window onClose={onClose}>
+      <Window onClose={onClose} appName="Notepad" appIcon={notepad_icon}>
         <div className={s.appContainer}>
           <section className={s.toolbar}>
             <div className={s.toolbarList}>
