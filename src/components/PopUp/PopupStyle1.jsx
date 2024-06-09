17bd3cb61bc8b01951aef2941ed9662a4004caa5
Lilian <lilianpeuron@gmail.com>
2024-06-09 17:12:09 +0200

    Upd popups audio

--- src/components/PopUp/PopupStyle1.jsx
+++ src/components/PopUp/PopupStyle1.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Window from "../Window/Window";
 import s from "./PopUp.module.scss";
 import audiovisualiser_icon from "../../assets/WindowsIcons/audiovisualiser_icon.png";
-import { Howl, Howler } from "howler";
 
 const PopupStyle1 = ({ onClose }) => {
-  // let audio = new Audio("/audios/chocapic.mp3");
-  // audio.play();
+  useEffect(() => {
+    const audio = new Audio("/audios/chocapic.mp3");
+    audio.play();
+
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, []);
 
   return (
     <Window
